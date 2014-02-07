476c9b0d7877a23b9a7be47265c783cd4465e5c9
Mark Fielbig <mfielbig@gmail.com>
2014-02-07 17:35:26 -0500

    Money and resources in model.

--- js/model.js
+++ js/model.js
@@ -1,12 +1,16 @@
 (function (global) {
 
+  var INITIAL_MONEY = 100;
+  var INITIAL_RESOURCES = 3;
+
   /**
 	 * Game model stuff goes here
 	 **/
 	var Game = {
-
+    money: INITIAL_MONEY,
+    resources: INITIAL_RESOURCES
 	};
-  
+
 	global.Game = Game;
 
 })(this)
