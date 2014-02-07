2e7f59dcc2c5d8e23991278c8f5e291f6242c56a
Mark Fielbig <mfielbig@gmail.com>
2014-02-07 17:30:19 -0500

    Changed tabs and added self calling functions.

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,6 @@
-/**
- * Stuff for setting up the canvas goes here
- **/
-Game.canvas = new createjs.Stage('canvas');
+(function() {
+	/**
+	* Stuff for setting up the canvas goes here
+	**/
+	Game.canvas = new createjs.Stage('canvas');
+})();
--- js/model.js
+++ js/model.js
@@ -1,6 +1,12 @@
-/**
- * Game model stuff goes here
- **/
-var Game = {
+(function (global) {
 
-};
+	/**
+	* Game model stuff goes here
+	**/
+	var Game = {
+
+	};
+
+	global.Game = Game;
+
+})(this)
