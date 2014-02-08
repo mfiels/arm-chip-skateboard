b06fa843be9fbac94fa5bb87c2ae643e10e2392c
Angad Sidhu <asidhu@fudgeu.com>
2014-02-07 22:28:24 -0500

    added resource queue

--- js/model.js
+++ js/model.js
@@ -21,14 +21,34 @@ var Game = {
     createjs.Ticker.addEventListener('tick', this.canvas);
   },
   addAction: function(action, location){
-	this.data.currentActions.push(new Pair(action,location));
+	//search model for action and location pair
+	var added=false;
+	for(var i=0;i<this.data.currentActions.length;i++){
+		if(this.data.currentActions[i].action===action && this.data.currentActions[i].location===location){
+			this.data.currentActions[i].count++;
+			added=true;
+		}
+	}
+	if(!added)
+		this.data.currentActions.push(new Pair(action,location));
 	Resources.updateCurrentActions();
-  }
+  },
+  removeAction: function(index){
+	if(this.data.currentActions.length>index){
+		if(this.data.currentActions[index].count>1)
+			this.data.currentActions[index].count--;
+		else
+			this.data.currentActions.splice(index,1);
+	}
+	else return;
+	Resources.updateCurrentActions();
+  },
 };
 
 var Pair = function(initialAction,initialLocation){
 	this.action = initialAction;
 	this.location= initialLocation;
+	this.count=1;
 }
 
 
