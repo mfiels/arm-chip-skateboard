6eb94062c761d78120bc206c66f0b5a74ee643bd
Andriy Duyko <andriy@aduyko.com>
2014-02-07 20:45:30 -0500

    drawing a little image

--- js/map.js
+++ js/map.js
@@ -13,7 +13,10 @@ var Map = {
     var background = new createjs.Shape();
     background.graphics.beginFill("#FF0000").drawRect(0, 0, this.WIDTH, this.HEIGHT);
     this.surface.addChild(background);
-
+    var icon = new createjs.Bitmap(Game.data.images['Library']);
+    icon.x = 200;
+    icon.y = 200;
+    this.surface.addChild(icon);
     Game.canvas.addChild(this.surface);
   }
 };
