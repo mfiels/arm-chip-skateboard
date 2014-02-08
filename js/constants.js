c99f7eef8dbb949e62fabc143d51d904d85e4b69
Andriy Duyko <andriy@aduyko.com>
2014-02-07 19:25:44 -0500

    Data Model

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,26 @@
 var Constants = {
   INITIAL_MONEY: 100,
-  INITIAL_RESOURCES: 3
+  INITIAL_RESOURCES: 3,
+  ALL_LOCATIONS: {
+    'Library': {
+      Risk: 0,
+      Reward: 0,
+      Description: 'Library',
+      Image: 0,
+    },
+  },
+  INITIAL_LOCATIONS: [
+    'Library',
+  ],
+  ALL_ACTIONS: {
+    'Forgot': {
+      Risk: 0,
+      Resources: 1,
+      Description: 'Someone forgot to log off!',
+      Image: 0,
+    },
+  },
+  INITIAL_ACTIONS: [
+    'Forgot',
+  ],
 };
--- js/model.js
+++ js/model.js
@@ -2,11 +2,24 @@
  * Game model stuff goes here
  **/
 var Game = {
-  money: Constants.INITIAL_MONEY,
-  resources: Constants.INITIAL_RESOURCES
-  canvas: {}
+  canvas: {},
+  data: {},
 
   init: function(){
     this.canvas = new createjs.Stage('canvas');
-  }
+    this.data = new Data();
+  },
 };
+
+var Data = function(){
+  this.money= Constants.INITIAL_MONEY;
+  this.resources= Constants.INITIAL_RESOURCES;
+  this.days= 0;
+  this.locations= Constants.INITIAL_LOCATIONS;
+  this.actions= Constants.INITIAL_ACTIONS;
+  this.risk= 0;
+
+  this.currentResources= 0;
+  this.currentActions= [];
+}
+
